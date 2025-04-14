24041a1e4505c62d123a9ce5758e9e9595ab720b
Jyotsna Thakur <j.thakur2608@gmail.com>
2025-04-14 14:08:54 +0530

    Create SummaryPage.js

---
+++ SummaryPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SummaryPage = () => {
+  return (
+    <div className="summary-page">
+      <h1>Contract Summary</h1>
+      <p>Here is the summarized content of your contract.</p>
+    </div>
+  );
+};
+
+export default SummaryPage;
